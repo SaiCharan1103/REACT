e8129dbda6d8253edf9409fe309541bcb52158ce
SaiCharan1103 <saic460@gmail.com>
2021-12-20 12:07:37 +0530

    12

---
+++ hello-world/src/components/UserGreeting.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class UserGreeting extends Component {
+    render() {
+        return (
+            <div>
+                Welcome Sai
+                
+            </div>
+        )
+    }
+}
+
+export default UserGreeting
