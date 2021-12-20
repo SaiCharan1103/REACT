d96f357865f86e7e2663a5c2d9e3d04be28e295b
SaiCharan1103 <saic460@gmail.com>
2021-12-20 12:24:08 +0530

    12:24

--- hello-world/src/components/UserGreeting.js
+++ hello-world/src/components/UserGreeting.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react'
 
 class UserGreeting extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+                isLoggedIn: true
+            }
+        }
+    
     render() {
-        return (
-            <div>
-                Welcome Sai
+        return(
+        this.state.isLoggedIn && <div>Welcome Sai</div>)
+        //turnery Approach
+        // return(
+        //  this.state.isLoggedIn?
+        //  <div>Welcome Sai</div> :
+        //  <div>Welcome Guest</div>
+        // )
+        //2nd Method
+        // let message
+        // if (this.state.isLoggedIn) {
+        //     message = <div>Welcome Sai</div>    
+        // } else {
+        //     message = <div>Welcome Guest</div>
+        // }
+        // return <div>{message}</div>
+        //1st Method
+        // if(this.state.isLoggedIn){
+        //     return(
+        //         <div>Welcome Sai</div>
+        //     )
+        // }else{
+        //     return(
+        //         <div>Welcome Guest</div>
+        //     )
+        // }
+        // return (
+        //     <div>
+        //         <div>Welcome Sai</div>
+        //         <div>Welcome Guest</div>
                 
-            </div>
-        )
+        //     </div>
+        // )
     }
 }
 
