f8777c07c36960b06671c3fc44924e42e06e5ad8
SaiCharan1103 <saic460@gmail.com>
2021-12-27 14:56:18 +0530

    27

--- hello-world/src/components/Welcome.js
+++
@@ -1,9 +0,0 @@
-import React,{Component} from "react";
-class Welcome extends Component{
-    render(){
-        const {name,animename} = this.props
-        // const {state1,state2}=this.state
-        return (<h1>Welcome {name} a.k.a {animename}</h1>
-        )}
-}
-export default Welcome
