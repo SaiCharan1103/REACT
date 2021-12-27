f2d80cd7c74c982bbbcf86de50d99f8d1b386b59
SaiCharan1103 <saic460@gmail.com>
2021-12-27 14:48:11 +0530

    27

--- carwash-frontend/src/App.js
+++ carwash-frontend/src/App.js
@@ -1,12 +1,18 @@
-
+import React from 'react';
 import './App.css';
 import Appbar from './components/Appbar';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-       <Appbar />
-    </div>
+    <>
+    <Router>
+      <Appbar />
+      <Switch>
+        <Route path='/' excat />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
--- carwash-frontend/src/components/Appbar.js
+++ carwash-frontend/src/components/Appbar.js
@@ -1,9 +1,29 @@
-import React,{useState} from "react"
+import React,{useState} from "react";
+import {Link} from 'react-router-dom'
 function Appbar(){
+    const [click,setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closedMobileMenu = () => setClick(false);
+
     return(
         <>
         <nav className="navbar">
-            <div className="navbar-container"></div>
+            <div className="navbar-container">
+                <Link to='/' className="navbar-logo">
+                    SAI <i className="fab fa-typo3"   />            
+                 </Link>
+                 <div className="menu-icon">
+                     <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                 </div>
+                 <ul className={click? 'nav-menu active' : 'nav-menu'}>
+                     <li className="nav-item">
+                         <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+                             Home
+                         </Link>
+                     </li>
+                 </ul>
+
+            </div>
         </nav>
 
         </>
